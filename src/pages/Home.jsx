2eb8066b50da63262b1565f3ac1b0a201ca1885f
1skepta <prospaul999@gmail.com>
2024-12-30 17:03:13 +0000

    add chart component to display job graphs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Dashboard from "../components/Dashboard";
-import ApplicationsList from "../components/ApplicationsList";
 
 function Home() {
   return (
     <div>
       <Dashboard />
-      <ApplicationsList />
     </div>
   );
 }
